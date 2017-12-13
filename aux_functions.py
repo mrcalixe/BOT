9afa5ce3a6e1d8afe2d4a96d161e6894e82800fd
mrcalixe <carlos.pereira.fcp@gmail.com>
2017-12-13 18:46:48 +0000

    Documentação do código

--- aux_functions.py
+++ aux_functions.py
@@ -3,17 +3,19 @@
 
 import random, json
 import sqlite3 as sql
-# import pymongo as mongo
 
 def oneOf(arr):
+    # Simples função que escolhe aleatóriamente um elemento de um array
     rand_idx = random.randint(0,len(arr) - 1)
     return arr[rand_idx]
 
 class State:
+    # Futura classe que representará o estado do BOT
     def __init__(self):
         self.sys = 1
 
 class Knowledge_BD:
+    # Esta BD tem o conhecimento que o BOT adquiriu (numa parte inicial, só sobre música)
     def __init__(self, tables = False):
         self.db = sql.connect('Knowledge_DB/db')
         self.id = 0
@@ -29,6 +31,7 @@ class Knowledge_BD:
             self.db.commit()
             cursor.close()
 
+    # Esta função insere na base de dados informação sobre o conhecimento de um autor, ou música
     def insert_knowledge(self, author, band, year, music):
         cursor = self.db.cursor()
         cursor.execute('''
@@ -37,6 +40,7 @@ class Knowledge_BD:
         self.db.commit()
         cursor.close()
 
+    # Esta função vai à base de dados buscar informação sobre um autor
     def get_by_author(self, author):
         cursor = self.db.cursor()
         cursor.execute('''
@@ -48,10 +52,12 @@ class Knowledge_BD:
 
 
 class Users_DB:
+    # Dicionário que contém os utilizadores que já utilizaram o BOT
     def __init__(self):
         self.users = {}
         self.users.clear()
 
+    # Visto que não existe a função has_key no dicionário, basta tentar aceder à chave e caso haja uma exceção significa que não existe entrada
     def check_user(self, user):
         try:
             t = self.users[user]
@@ -59,14 +65,18 @@ class Users_DB:
         except KeyError:
             return False
 
+    # Adiciona uma entrada de um utilizador.
+    # Nesta entrada ainda vai existir um log com as vezes que este utilizador já utilizou o BOT, bem como o tema das conversas.
     def add_user(self, user):
         self.users[user] = {}
 
 
+# Esta função escreve num ficheiro em formato JSON o dicionário dos utilizadores
 def dump_users(users_db, file):
     with open(file, "w+") as outfile:
         json.dump(users_db, outfile)
 
+# Esta função lê de um ficheiro no formato JSON o dicionário dos utilizadores
 def readback_users(file):
     with open(file, "r") as outfile:
         data = json.load(outfile)
