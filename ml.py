979a78e88f5ab95838b380e09d40593074afed7e
mrcalixe <carlos.pereira.fcp@gmail.com>
2018-01-04 17:28:42 +0000

    Continuação da função de divisão silábica

--- ml.py
+++ ml.py
@@ -6,47 +6,112 @@ import csv, random, nltk, enchant
 
 vogais = 'aeiou'
 consoantes = 'qwrtypsdfghjklzxcvbnm'
+# Ditongos decrescentes
+ditongos_dec = ['ai', 'êi', 'ãi', 'ei', 'éi', 'oi', 'ói', 'ui', 'au', 'áu', 'eu', 'éu', 'iu', 'ou', 'ãe', 'ão', 'õe']
+comb = ['qu', 'gu'] # Combinações
+hiatos = ['ea', 'eo', 'ia', 'ie', 'io', 'oa', 'oe', 'ua', 'ue', 'ui', 'uo'] # Hiatos
+consonanticos = (['b', 'c', 'd', 'f', 'g', 'p', 't', 'v'], ['l', 'r'])
+digrafos = ['lh', 'nh', 'ch']
 
 
-# NAO ACABADA
-def divisao_silabas2(palavra):
-    if len(palavra) == 0:
-        return []
-    elif 1 <= len(palavra) <= 2:
-        return [palavra]
-    elif True:
-        return []
-
-
-# ERRADA - A tentar construir de forma recursiva pois é mais acertada e simples
 def divisao_silabas(palavra):
-    silabas = []
-    limite = len(palavra)-1
-    aux = ''
+    s = [] # Sílabas
     i = 0
+    j = 0
     while i < len(palavra):
-        aux = aux+palavra[i]
-        if palavra[i+1] in consoantes and palavra[i+2] in consoantes:
-            aux = aux+palavra[i+1]
-            i += 2
-            silabas.append(aux)
-            aux = ''
-        elif palavra[i+1] in consoantes and palavra[i+2] in vogais:
-            #aux = aux + pala vra[i]
-            i += 1
-            silabas.append(aux)
-            aux = ''
-        elif palavra[i+1] in vogais and palavra[i+2] in consoantes:
-            #aux = aux + palavra[i]
-            silabas.append(aux)
-            aux = ''+palavra[i+1]
-            i += 2
-            silabas.append(aux)
-            aux = ''
+        print("Estado - i:", i, "; j:", j, "; silabas [",s,"]")
+        a = input("Continuar?")
+        # Quando a 1ª palavra é consoante
+        if palavra[i] in consoantes:
+            # Caso apareça uma vogal, incrementa-se o i
+            # para que o algorítmo passe pelos casos das vogais
+            if palavra[i+1] in vogais:
+                if palavra[i+1] == 'u' and palavra[i] in ['q','g']:
+                    # Caso apareça um ditongo - divide-se com o ditongo
+                    if (palavra[i+2]+palavra[i+3]) in ditongos_dec:
+                        s.append(palavra[j : i+4])
+                        i += 4
+                        j = i
+                    # Caso apareça uma vogal só, divide-se com a vogal
+                    elif palavra[i+2] in vogais:
+                        s.append(palavra[j : i+3])
+                        i += 3
+                        j = i
+                elif i+2 == len(palavra) and palavra[i+2] in consoantes:
+                    s.append(palavra[j : i+2])
+                    i += 2
+                    j = i
+                    # Hiatos na silaba final
+                elif (palavra[i] + palavra[i + 1]) in hiatos and i + 2 == len(palavra):
+                    s.append(palavra[j: i + 3])
+                    i += 3
+                    j = i
+                else:
+                    i += 1
+            # Caso de consoantes seguidas
+            elif palavra[i+1] in consoantes:
+                # Digrafos - Indivisível
+                if (palavra[i]+palavra[i+1]) in digrafos:
+                    i += 2
+                # Grupo consonântico
+                elif palavra[i] in consonanticos[0] and palavra[i+1] in consonanticos[1]:
+                    # No prefixo, divisível
+                    if len(s) == 0:
+                        s.append(palavra[j : i+1])
+                        i += 1
+                        j = i
+                    # Caso contrário, indivisível
+                    else:
+                        i += 2
+                # Caso apareça cz ou ps no início da silaba - Indivisível
+                elif len(s) == 0 and i==0 and (palavra[i]+palavra[i+1]) in ['cz', 'ps']:
+                    i += 2
+                # Caso contrário, onde se divide
+                else:
+                    s.append(palavra[j : i+1])
+                    i += 1
+                    j = i
+        # Quando a 1ª palavra é vogal
         else:
-            aux = ''
-            break
-    return silabas
+            # Sequência de vogais
+            if i+1 == len(palavra):
+                s.append(palavra[j : i+1])
+                i += 1
+                j = i
+            elif palavra[i+1] in vogais:
+                # ditongos decrescentes - Indivisível
+                if (palavra[i]+palavra[i+1]) in ditongos_dec:
+                    i += 2
+                # Hiatos na silaba final
+                elif (palavra[i]+palavra[i+1]) in hiatos and i+2 == len(palavra):
+                    s.append(palavra[j : i+2])
+                    i += 2
+                    j = i
+                else:
+                    s.append(palavra[j : i+1])
+                    i += 1
+                    j = i
+            # Aparece uma consoante
+            else:
+                # Caso venha uma outra consoante que nao sejam consonanticas
+                # Grupo consonântico
+                if palavra[i+1] in consonanticos[0] and palavra[i+2] in consonanticos[1]:
+                    # No prefixo, divisível
+                    if len(s) == 0:
+                        s.append(palavra[j: i + 2])
+                        i += 2
+                        j = i
+                    # Caso contrário, indivisível
+                    else:
+                        i += 3
+                # Caso contrário, onde se divide
+                else:
+                    s.append(palavra[j: i + 1])
+                    i += 1
+                    j = i
+    #Ao testar com admirador dá erro
+    return s
+
 
 
 def gender_feature(word):
---
+++ ml_floresta.py
@@ -0,0 +1,4 @@
+import csv, random, nltk, enchant
+from nltk.corpus import floresta
+
+floresta.tagged_words()
